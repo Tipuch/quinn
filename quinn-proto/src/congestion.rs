c291e18121a3043efc1bb788a872ef5180dff035
Tipuch <fiorini751@proton.me>
2026-01-13 21:05:14 +0700

    added send_quantum to controller metrics, changed pacing_rate from bits/s to bytes/s

--- quinn-proto/src/congestion.rs
+++ quinn-proto/src/congestion.rs
@@ -96,11 +96,14 @@ pub trait Controller: Send + Sync {
     fn window(&self) -> u64;
 
     /// Retrieve implementation-specific metrics used to populate `qlog` traces when they are enabled
+    /// This is also used to alter the pacing of the connection with
+    /// `pacing_rate` and `send_quantum`
     fn metrics(&self) -> ControllerMetrics {
         ControllerMetrics {
             congestion_window: self.window(),
             ssthresh: None,
             pacing_rate: None,
+            send_quantum: None,
         }
     }
 
@@ -114,7 +117,9 @@ pub trait Controller: Send + Sync {
     fn into_any(self: Box<Self>) -> Box<dyn Any>;
 }
 
-/// Common congestion controller metrics
+/// Common congestion controller metrics used both for logging purposes
+/// but also to alter the pacing of the connection with
+/// `pacing_rate` and `send_quantum`
 #[derive(Default)]
 #[non_exhaustive]
 pub struct ControllerMetrics {
@@ -122,8 +127,10 @@ pub struct ControllerMetrics {
     pub congestion_window: u64,
     /// Slow start threshold (bytes)
     pub ssthresh: Option<u64>,
-    /// Pacing rate (bits/s)
+    /// Pacing rate (bytes/s)
     pub pacing_rate: Option<u64>,
+    /// Send Quantum (bytes) used to control the size of packet bursts
+    pub send_quantum: Option<u64>,
 }
 
 /// Constructs controllers on demand
